3da1c676a7ceb8c508f196e08cfb8550bb47677f
muscleleg <51969892+muscleleg@users.noreply.github.com>
2022-09-20 04:26:03 +0900

    백업0919

--- src/main/java/jpabook/InitDb.java
+++ src/main/java/jpabook/InitDb.java
@@ -1,13 +1,10 @@
 package jpabook;
 
-import jpabook.jpashop.domain.Address;
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.*;
 import jpabook.jpashop.domain.item.Book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
@@ -18,7 +15,9 @@ public class InitDb {
     private final InitService initService;
     @PostConstruct
     public void init() {
+        
         initService.doInit1();
+        initService.doInit2();
     }
     @Component
     @Transactional
@@ -26,9 +25,7 @@ public class InitDb {
     static class InitService {
         private final EntityManager em;
         public void doInit1() {
-            Member member = new Member();
-            member.setName("userA");
-            member.setAddress(new Address("서울", "1", "1111"));
+            Member member = createMember("userA", "서울", "1", "1111");
             em.persist(member);
 
             Book book1 = new Book();
@@ -43,8 +40,47 @@ public class InitDb {
             book2.setStockQuantity(100);
             em.persist(book2);
 
-            OrderItem.createOrderItem(book1, 10000, 1);
-            OrderItem.createOrderItem(book2, 20000, 2);
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
+
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+
+        }
+
+        private Member createMember(String name, String city, String street, String zipcode) {
+            Member member = new Member();
+            member.setName(name);
+            member.setAddress(new Address(city, street, zipcode));
+            return member;
+        }
+
+        public void doInit2() {
+            Member member = createMember("userB", "진주", "2", "2222");
+            em.persist(member);
+
+            Book book1 = new Book();
+            book1.setName("JPA1 BOOk");
+            book1.setPrice(10000);
+            book1.setStockQuantity(100);
+            em.persist(book1);
+
+            Book book2 = new Book();
+            book2.setName("JPA2 BOOK");
+            book2.setPrice(20000);
+            book2.setStockQuantity(100);
+            em.persist(book2);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
+
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+
         }
     }
 }
